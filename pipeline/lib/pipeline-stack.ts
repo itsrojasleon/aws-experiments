86f9d0db0cc51d72de44fb636ad39ec2344bfb97
rojasleon <rojasleon.dev@gmail.com>
2022-11-05 16:16:52 -0700

    Add pipeline example

--- pipeline/lib/pipeline-stack.ts
+++ pipeline/lib/pipeline-stack.ts
@@ -1,24 +1,79 @@
 import * as cdk from 'aws-cdk-lib';
-import {
-  CodePipeline,
-  CodePipelineSource,
-  ShellStep
-} from 'aws-cdk-lib/pipelines';
+import { SecretValue } from 'aws-cdk-lib';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codepipelineActions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class PipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'MyPipeline',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub(
-          'rojasleon/simple-ts-lambda-layer',
-          'main'
-        ),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+    const artifact = new codepipeline.Artifact();
+
+    const sourceAction = new codepipelineActions.GitHubSourceAction({
+      actionName: 'GitHub_Source',
+      owner: 'rojasleon',
+      repo: 'simple-ts-lambda-layer',
+      // Github token must be created manually.
+      oauthToken: SecretValue.secretsManager('my-github-token'),
+      output: artifact,
+      branch: 'main'
+    });
+
+    const project = new codebuild.Project(this, 'project', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_6_0,
+        computeType: codebuild.ComputeType.MEDIUM
+      },
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: 0.2,
+        phases: {
+          install: {
+            'runtime-versions': {
+              nodejs: '16.x'
+            },
+            commands: ['npm install']
+          },
+          // pre_build: {
+          //   commands: [
+          //     `cd service && touch .env && echo AWS_REGION=${
+          //       props!.env!.region
+          //     } >> .env && echo AWS_ACCOUNT_ID=${props!.env!.account} >> .env`
+          //   ]
+          // },
+          build: {
+            commands: ['npm run deploy']
+          }
+        }
       })
+    });
+
+    project.addToRolePolicy(
+      new iam.PolicyStatement({
+        resources: ['*'],
+        actions: ['*']
+      })
+    );
+
+    const codeBuildAction = new codepipelineActions.CodeBuildAction({
+      actionName: 'codeBuildAction',
+      project,
+      input: artifact
+    });
+
+    new codepipeline.Pipeline(this, 'pipeline', {
+      stages: [
+        {
+          stageName: 'Source',
+          actions: [sourceAction]
+        },
+        {
+          stageName: 'Deploy',
+          actions: [codeBuildAction]
+        }
+      ]
     });
   }
 }
